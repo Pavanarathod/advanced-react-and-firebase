7728b3b8e8728daec6782803061a209a7d8a17b0
Pavan Rathod <edroculllen13@gmail.com>
2021-09-30 09:58:14 +0530

    commit from pavan

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { auth } from "./database/firebase";
 import DevelopmentPage from "./pages/DevelopmentPage";
 import HomePage from "./pages/HomePage";
+import PavanPage from "./pages/PavanPage";
 import RegisterPage from "./pages/RegisterPage";
 import UpdatePage from "./pages/UpdatePage";
 
@@ -17,6 +18,7 @@ function App() {
             <Route path="/update/:id" component={UpdatePage} />
             <Route exact path="/" component={HomePage} />
             <Route path="/register" component={DevelopmentPage} />
+            <Route path="/pavan" component={PavanPage} />
           </Switch>
         </div>
       ) : (
---
+++ src/pages/PavanPage.js
@@ -0,0 +1,5 @@
+const PavanPage = () => {
+  return <div>this is my page of development</div>;
+};
+
+export default PavanPage;
